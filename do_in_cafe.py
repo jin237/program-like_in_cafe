b6a0918016cc1cb94b8f6c0b556903eb4afc2174
takehiro iino <50320445+jin237@users.noreply.github.com>
2021-03-20 17:17:23 +0900

    Add files via upload

--- do_in_cafe.py
+++ do_in_cafe.py
@@ -1,11 +1,16 @@
 import random
 import time
+import shutil
+
+
+terminal_size = shutil.get_terminal_size()
+
 
 value = list()
 for i in range(50):
-    value.append(random.randint(10**79,10**80))
+    value.append(random.randint(10**(terminal_size.columns-1),10**terminal_size.columns))
 
-for k in range(1000):
+for k in range(5000):
     x = random.randint(0,49)
     print(value[x])
     time.sleep(0.2)
